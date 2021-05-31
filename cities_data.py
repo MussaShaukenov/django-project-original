d3995726eeb8faa371e7ac58e653da090bf6f41f
Mussa <66770364+MussaShaukenov@users.noreply.github.com>
2021-05-31 07:50:12 +0500

    Update cities_data.py

--- cities_data.py
+++ cities_data.py
@@ -22,91 +22,91 @@ data_02 = {'NoI current': current_situation[1]['NoI'],
            'NoR expected': future_situation[1]['NoR']}
 
 #  Shymkent Data
-shymkent_13 = {'NoI current': current_situation[3]['NoI'],
-               'NoR current': current_situation[3]['NoR'],
-               'NoI expected': future_situation[3]['NoI'],
-               'NoR expected': future_situation[3]['NoR']}
+shymkent_13 = {'NoI current': current_situation[2]['NoI'],
+               'NoR current': current_situation[2]['NoR'],
+               'NoI expected': future_situation[2]['NoI'],
+               'NoR expected': future_situation[2]['NoR']}
 
 #  Akmola Region Data
-data_03 = {'NoI current': current_situation[4]['NoI'],
+data_03 = {'NoI current': current_situation[3]['NoI'],
+           'NoR current': current_situation[3]['NoR'],
+           'NoI expected': future_situation[3]['NoI'],
+           'NoR expected': future_situation[3]['NoR']}
+
+#  Aktobe Region Data
+data_04 = {'NoI current': current_situation[4]['NoI'],
            'NoR current': current_situation[4]['NoR'],
            'NoI expected': future_situation[4]['NoI'],
            'NoR expected': future_situation[4]['NoR']}
 
-#  Aktobe Region Data
-data_04 = {'NoI current': current_situation[5]['NoI'],
+#  Almaty Region Data
+data_05 = {'NoI current': current_situation[5]['NoI'],
            'NoR current': current_situation[5]['NoR'],
            'NoI expected': future_situation[5]['NoI'],
            'NoR expected': future_situation[5]['NoR']}
 
-#  Almaty Region Data
-data_05 = {'NoI current': current_situation[6]['NoI'],
+#  Atyrau Region Data
+data_06 = {'NoI current': current_situation[6]['NoI'],
            'NoR current': current_situation[6]['NoR'],
            'NoI expected': future_situation[6]['NoI'],
            'NoR expected': future_situation[6]['NoR']}
 
-#  Atyrau Region Data
-data_06 = {'NoI current': current_situation[7]['NoI'],
+#  VKO Data
+data_16 = {'NoI current': current_situation[7]['NoI'],
            'NoR current': current_situation[7]['NoR'],
            'NoI expected': future_situation[7]['NoI'],
            'NoR expected': future_situation[7]['NoR']}
 
-#  VKO Data
-data_16 = {'NoI current': current_situation[8]['NoI'],
+#  Zhambyl Region Data
+data_08 = {'NoI current': current_situation[8]['NoI'],
            'NoR current': current_situation[8]['NoR'],
            'NoI expected': future_situation[8]['NoI'],
            'NoR expected': future_situation[8]['NoR']}
 
-#  Zhambyl Region Data
-data_08 = {'NoI current': current_situation[9]['NoI'],
+#  3KO Data
+data_07 = {'NoI current': current_situation[9]['NoI'],
            'NoR current': current_situation[9]['NoR'],
            'NoI expected': future_situation[9]['NoI'],
            'NoR expected': future_situation[9]['NoR']}
 
-#  3KO Data
-data_07 = {'NoI current': current_situation[10]['NoI'],
+#  Qaragandy Region Data
+data_09 = {'NoI current': current_situation[10]['NoI'],
            'NoR current': current_situation[10]['NoR'],
            'NoI expected': future_situation[10]['NoI'],
            'NoR expected': future_situation[10]['NoR']}
 
-#  Qaragandy Region Data
-data_09 = {'NoI current': current_situation[11]['NoI'],
+#  Qostanay Region Data
+data_10 = {'NoI current': current_situation[11]['NoI'],
            'NoR current': current_situation[11]['NoR'],
            'NoI expected': future_situation[11]['NoI'],
            'NoR expected': future_situation[11]['NoR']}
 
-#  Qostanay Region Data
-data_10 = {'NoI current': current_situation[12]['NoI'],
+#  Qyzylorda Region Data
+data_11 = {'NoI current': current_situation[12]['NoI'],
            'NoR current': current_situation[12]['NoR'],
            'NoI expected': future_situation[12]['NoI'],
            'NoR expected': future_situation[12]['NoR']}
 
-#  Qyzylorda Region Data
-data_11 = {'NoI current': current_situation[13]['NoI'],
+#  Mangystau Region Data
+data_12 = {'NoI current': current_situation[13]['NoI'],
            'NoR current': current_situation[13]['NoR'],
            'NoI expected': future_situation[13]['NoI'],
            'NoR expected': future_situation[13]['NoR']}
 
-#  Mangystau Region Data
-data_12 = {'NoI current': current_situation[14]['NoI'],
+#  Pavlodar Region Data
+data_14 = {'NoI current': current_situation[14]['NoI'],
            'NoR current': current_situation[14]['NoR'],
            'NoI expected': future_situation[14]['NoI'],
            'NoR expected': future_situation[14]['NoR']}
 
-#  Pavlodar Region Data
-data_14 = {'NoI current': current_situation[15]['NoI'],
+#  CKO Region Data
+data_15 = {'NoI current': current_situation[15]['NoI'],
            'NoR current': current_situation[15]['NoR'],
            'NoI expected': future_situation[15]['NoI'],
            'NoR expected': future_situation[15]['NoR']}
 
-#  CKO Region Data
-data_15 = {'NoI current': current_situation[16]['NoI'],
+#  Turkestan Region Data
+data_13 = {'NoI current': current_situation[16]['NoI'],
            'NoR current': current_situation[16]['NoR'],
            'NoI expected': future_situation[16]['NoI'],
            'NoR expected': future_situation[16]['NoR']}
-
-#  Turkestan Region Data
-data_13 = {'NoI current': current_situation[17]['NoI'],
-           'NoR current': current_situation[17]['NoR'],
-           'NoI expected': future_situation[17]['NoI'],
-           'NoR expected': future_situation[17]['NoR']}
